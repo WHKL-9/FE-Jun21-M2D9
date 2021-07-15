b144996cc745f9cc734de34c000bcec0f811971d
WHKL-9 <64627811+WHKL-9@users.noreply.github.com>
2021-07-15 16:56:03 +0200

    Completed ex1 till ex7

---
+++ index.js
@@ -0,0 +1,32 @@
+// delete row button
+const deleteRow1 = function (){
+    //get the row 
+    let getTargetRow = document.getElementById("row1")
+    getTargetRow.remove()
+}
+
+const deleteRow2 = function (){
+    //get the row 
+    let getTargetRow = document.getElementById("row2")
+    getTargetRow.remove()
+}
+
+const deleteRow3 = function (){
+    //get the row 
+    let getTargetRow = document.getElementById("row3")
+    getTargetRow.remove()
+}
+
+const deleteRow4 = function (){
+    //get the row 
+    let getTargetRow = document.getElementById("row4")
+    getTargetRow.remove()
+}
+
+const deleteRow5 = function (){
+    //get the row 
+    let getTargetRow = document.getElementById("row5")
+    getTargetRow.remove()
+}
+
+const 
